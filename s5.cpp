6de47958f89a41d7a622d3027de91fbda0b6b02d
Swathi Mudakavi <swathimudakavi06@gmail.com>
2026-09-02 19:11:29 +0530

    Initial commit

---
+++ s5.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+void logMsg(const string &msg, int level=1){
+const string tag[] = {" ","INFO","WARN","ERROR"};
+cout << "[" << tag[level] << "]" << msg << endl;
+}
+
+double interest(double principal, double years, double rate=7.5){
+return principal * rate * years/100.0;
+}
+
+int main(){
+logMsg("system started");
+logMsg("low memory",2);
+cout << "Interest = " << interest(10000,2)<<endl;
+cout << "Interest = " << interest(10000,2,9.0)<<endl;
+return 0;
+}
